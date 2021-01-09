d2009e144a6e34a2fc8452ae7d919f34a9cf771b
Jörg Hohwiller <hohwille@users.sourceforge.net>
2021-01-09 01:01:37 +0100

    resolved warnings

--- factory/src/main/java/module-info.java
+++ factory/src/main/java/module-info.java
@@ -3,13 +3,13 @@
  * Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0
  */
-import io.github.mmm.bean.BeanFactory;
-import io.github.mmm.bean.factory.impl.BeanFactoryImpl;
 
 /**
  * Provides {@link io.github.mmm.bean.BeanFactory} to create beans also from interfaces as dynamic proxies. If you want
  * to have no boilerplate code at all, like to have multi-inheritance and do not fear magic, you can even define your
  * beans as interfaces. To instantiate such bean, you need to use {@link io.github.mmm.bean.BeanFactory#create(Class)}.
+ *
+ * @provides io.github.mmm.bean.BeanFactory
  */
 // module should actually not be open - just a temporary workaround for tooling bug or JPMS design flaw
 // https://bugs.eclipse.org/bugs/show_bug.cgi?id=536847
@@ -17,6 +17,6 @@ open module io.github.mmm.bean.factory {
 
   requires transitive io.github.mmm.bean;
 
-  provides BeanFactory with BeanFactoryImpl;
+  provides io.github.mmm.bean.BeanFactory with io.github.mmm.bean.factory.impl.BeanFactoryImpl;
 
 }
