a1deedf1882e829d477fc1e1c1f0013c06ee77dd
Jörg Hohwiller <joerg.hohwiller@capgemini.com>
2023-06-10 17:32:12 +0200

    added initial value constructor

--- core/src/main/java/io/github/mmm/bean/property/BeanProperty.java
+++ core/src/main/java/io/github/mmm/bean/property/BeanProperty.java
@@ -2,6 +2,8 @@
  * http://www.apache.org/licenses/LICENSE-2.0 */
 package io.github.mmm.bean.property;
 
+import java.util.Objects;
+
 import io.github.mmm.bean.AbstractBean;
 import io.github.mmm.bean.BeanHelper;
 import io.github.mmm.bean.WritableBean;
@@ -48,6 +50,33 @@ public class BeanProperty<V extends WritableBean> extends Property<V> implements
 
     super(name, metadata);
     this.valueClass = valueClass;
+  }
+
+  /**
+   * The constructor.
+   *
+   * @param name the {@link #getName() name}.
+   * @param value the (initial) {@link #get() value}.
+   */
+  public BeanProperty(String name, V value) {
+
+    this(name, value, null);
+  }
+
+  /**
+   * The constructor.
+   *
+   * @param name the {@link #getName() name}.
+   * @param value the (initial) {@link #get() value}.
+   * @param metadata the {@link #getMetadata() metadata}.
+   */
+  @SuppressWarnings("unchecked")
+  public BeanProperty(String name, V value, PropertyMetadata<V> metadata) {
+
+    super(name, metadata);
+    Objects.requireNonNull(value);
+    this.value = value;
+    this.valueClass = (Class<V>) this.value.getClass();
   }
 
   @Override
