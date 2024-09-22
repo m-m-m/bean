bdc227a3ac1c45bb01036b2e528d3463c258cfc6
Jörg Hohwiller <joerg.hohwiller@capgemini.com>
2024-09-22 19:28:44 +0200

    fixed JavaDoc link

--- generator/src/main/java/module-info.java
+++ generator/src/main/java/module-info.java
@@ -4,7 +4,7 @@
  */
 
 /**
- * Provides a generator for all {@link WritableBean bean interfaces} on classpath alongside with
+ * Provides a generator for all {@link io.github.mmm.bean.WritableBean bean interfaces} on classpath alongside with
  * {@link io.github.mmm.bean.BeanFactory} to create instances of these beans. Allows ahead-of-time (AOT) compilation for
  * environments where reflection is not available (GraalVM, TeaVM, etc.).
  */
